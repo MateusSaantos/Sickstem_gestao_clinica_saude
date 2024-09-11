6b12de1815e1b72bce4b8879d842eca67ed42b8f
MateusSaantos <mateus_saantos@outlook.com>
2024-09-11 17:41:06 -0300

    Conec com a base de dados SINGLETON

--- src/database/database.php
+++ src/database/database.php
@@ -1,4 +1,5 @@
 <?php
+//Conexão com o Banco de dados respeitando o padrão de projeto Singleton
 
 namespace App\Database;
 
@@ -6,13 +7,19 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 class Database
 {
+    // Variável estática que armazenará a única instância da classe Database
     private static $instance = null;
+    
+    // Propriedade que armazenará a instância do gerenciador de conexões Capsule
     private $capsule;
 
+    // Construtor privado impede que a classe seja instanciada diretamente
     private function __construct()
     {
+        // Cria uma nova instância do gerenciador de conexões Capsule
         $this->capsule = new Capsule;
 
+        // Configura a conexão com o banco de dados usando as configurações fornecidas
         $this->capsule->addConnection([
             'driver'    => 'mysql',
             'host'      => '127.0.0.1',
@@ -24,22 +31,27 @@ class Database
             'prefix'    => '',
         ]);
 
-        // Make this Capsule instance available globally via static methods
+        // Torna a instância do Capsule globalmente acessível através de métodos estáticos
         $this->capsule->setAsGlobal();
 
-        // Setup the Eloquent ORM
+        // Configura o ORM Eloquent para ser usado
         $this->capsule->bootEloquent();
     }
 
+    // Método estático que retorna a única instância da classe Database
     public static function getInstance()
     {
+        // Verifica se a instância já foi criada
         if (self::$instance === null) {
+            // Se ainda não foi criada, cria uma nova instância
             self::$instance = new Database();
         }
 
+        // Retorna a instância única da classe Database
         return self::$instance;
     }
 
+    // Método para obter a instância do Capsule, facilitando o acesso à conexão com o banco de dados
     public function getCapsule()
     {
         return $this->capsule;
