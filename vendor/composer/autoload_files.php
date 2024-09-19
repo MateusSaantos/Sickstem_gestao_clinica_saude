e346a33b7b43667fd35333568a7a842aa81fb201
MateusSaantos <mateus_saantos@outlook.com>
2024-09-19 10:53:16 -0300

    Configuração ambiente testes PHPUnit

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -12,4 +12,6 @@ return array(
     'a4a119a56e50fbb293281d9a48007e0e' => $vendorDir . '/symfony/polyfill-php80/bootstrap.php',
     'a1105708a18b76903365ca1c4aa61b02' => $vendorDir . '/symfony/translation/Resources/functions.php',
     '72579e7bd17821bb1321b87411366eae' => $vendorDir . '/illuminate/support/helpers.php',
+    '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
+    'ec07570ca5a812141189b1fa81503674' => $vendorDir . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
 );
